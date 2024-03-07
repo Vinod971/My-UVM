839bac6330762d08c86ce76cae7c43b51572498d
Vinod Kumar Bandela <vinodk@mo.ece.pdx.edu>
2024-03-06 21:11:11 -0800

    Update Write Driver and Transcation

--- src/tb/Write_driver.sv
+++ src/tb/Write_driver.sv
@@ -2,30 +2,42 @@ class write_drv extends uvm_driver#(write_tx);
  `uvm_component_utils(write_drv)
  `NEW
  virtual interface_fifo inf;
-
+ int h = 0;
+ int G;
  function void build_phase(uvm_phase phase);
  super.build_phase(phase);
  if(!uvm_resource_db#(virtual interface_fifo)::read_by_name("ALL","TB",inf,this))
  begin
  `uvm_error("WRITE_DRIVER","Problem with the interface")
  end
- 
+  if(!uvm_resource_db#(int)::read_by_name("GOBAL","GEN",G,this)) begin
+		`uvm_error("WRITE_Seq","Problem with the interface")
+	 end
  endfunction
 
  task run_phase(uvm_phase phase);
   wait(inf.wrst_n == 1);
   forever begin 
-	seq_item_port.get_next_item(req);
+	 seq_item_port.get_next_item(req);
     drive_tx(req);
-	// req.print();
+	 //req.print();
     seq_item_port.item_done;
    end
+
  endtask
 
+ //Driving inputs to the DUT
 task drive_tx(write_tx tx);
 		@(posedge inf.wclk);
 		inf.w_en =1;
 		inf.data_in = tx.data;
+    h++;
+    if(h == G) begin
+     @(posedge inf.wclk);
+     
+     inf.w_en =0;
+     inf.data_in = 0;
+     end
   `uvm_info("WRITE_DRIVER", $sformatf("Write_EN = %0d, Data_in = %2h", inf.w_en, tx.data), UVM_NONE)
 endtask
 
--- src/tb/Write_tx.sv
+++ src/tb/Write_tx.sv
@@ -1,5 +1,8 @@
 class write_tx extends uvm_sequence_item;
-  rand bit [7:0] data;
+  randc bit [7:0] data;
+  logic wrst_n;
+  logic w_en;
+  logic full, write_error;
 
  `uvm_object_utils_begin(write_tx)
  `uvm_field_int(data,UVM_ALL_ON)
